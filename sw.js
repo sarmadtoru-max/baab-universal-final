5970b9a34dd74514d397aac550ed1d8d819d4990
sarmadtoru-max <sarmadtoru@gmail.com>
2026-09-21 12:18:28 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e=>{
- e.waitUntil(caches.open('king-v9').then(c=>c.addAll(['/'])))
-});
-self.addEventListener('fetch', e=>{
- e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)))
-});
